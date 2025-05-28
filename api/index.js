752ab26d5378f3414b4a310677f9e9e2f85dad94
Ma. Kristine R. Mien <mcmien@gbox.adnu.edu.ph>
2025-05-28 15:28:54 +0800

    update: installed npm install express

--- api/index.js
+++ api/index.js
@@ -14,7 +14,11 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(cors({
+  credentials: true,
+  origin: ["https://test-mern-wine.vercel.app"], // 🔥 Replace with your actual Vercel URL!
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
