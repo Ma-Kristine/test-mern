122e36214e384d99a907157efaf78a5168e9e80b
Ma. Kristine R. Mien <mcmien@gbox.adnu.edu.ph>
2025-05-28 10:01:15 +0800

    update: config.js

--- client/src/api/config.js
+++
@@ -1,2 +0,0 @@
-export const backendUrl = "https://replit.com/@Ma-Kristine-RKr/my-blog-site";
-// updates
---
+++ client/src/config.js
@@ -0,0 +1,2 @@
+const backendUrl = "https://replit.com/@Ma-Kristine-RKr/my-blog-site";
+fetch(`${backendUrl}/your-endpoint`);
